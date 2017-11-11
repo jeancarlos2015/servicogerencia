6435c896583bee9c9fbcc9c9d8629d6f82a9790f
jean <jean@gmail.com>
2017-11-11 13:48:20 -0200

    habilitando portas para acesso api

--- src/main/java/com/sistex/App.java
+++ src/main/java/com/sistex/App.java
@@ -1,11 +1,31 @@
 package com.sistex;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @SpringBootApplication
+@EnableAutoConfiguration
+@ComponentScan
 public class App {
-
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurerAdapter() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/fornecedor").allowedOrigins("http://localhost:9000");
+                registry.addMapping("/gerente").allowedOrigins("http://localhost:9000");
+                
+                registry.addMapping("/fornecedor").allowedOrigins("http://localhost:8080");
+                registry.addMapping("/gerente").allowedOrigins("http://localhost:8080");
+            }
+        };
+    }
 	public static void main(String[] args) {
 		SpringApplication.run(App.class, args);
 	}
