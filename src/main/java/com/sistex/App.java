a1137cdf45c2ac9afec71a9deb571c7bdda5490f
jean <jean@gmail.com>
2017-11-12 22:51:31 -0200

    habilitando portas

--- src/main/java/com/sistex/App.java
+++ src/main/java/com/sistex/App.java
@@ -10,18 +10,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @SpringBootApplication
-@EnableAutoConfiguration
-@ComponentScan
 public class App {
-    @Bean
-    public WebMvcConfigurer corsConfigurer() {
-        return new WebMvcConfigurerAdapter() {
-            @Override
-            public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/**");
-            }
-        };
-    }
 	public static void main(String[] args) {
 		SpringApplication.run(App.class, args);
 	}
