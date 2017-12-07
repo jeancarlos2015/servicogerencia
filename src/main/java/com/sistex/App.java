6af2cbe0f2d3b1c13f0cba1047e7976f6f235012
jean <jeancarlospenas28@gmail.com>
2017-12-07 10:48:03 -0200

    habilitando jparepositorios

--- src/main/java/com/sistex/App.java
+++ src/main/java/com/sistex/App.java
@@ -4,10 +4,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
 @ComponentScan
 @EnableAutoConfiguration
+@EnableJpaRepositories
 public class App {
 	public static void main(String[] args) {
 		SpringApplication.run(App.class, args);
